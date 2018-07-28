c8c5fc1b685e1c18df4c064eee7235abe7df97aa
Huzeifa.Ezzi <huzee007@gmail.com>
2018-07-29 00:32:41 +0530

    bridge and tempate

---
+++ Patterns/Behavioral/TemplateMethod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Behavioral
+{
+    /// <summary>
+    /// Define the skeleton of an algorithm in an operation, deferring some steps to subclasses. 
+    /// Template Method lets subclasses redefine certain steps of an algorithm without changing the algorithm's structure
+    /// </summary>
+    abstract class Record
+    {
+        public void Save()//template method
+        {
+            if (this.Validate())
+            {
+                this.BeforeSave();
+                //run DB query
+                this.AfterSave();
+            }
+        }
+
+        internal abstract void AfterSave();
+        internal abstract void BeforeSave();
+        internal abstract bool Validate();
+    }
+
+    class User : Record
+    {
+        internal override void AfterSave()
+        {
+            //do something
+        }
+
+        internal override void BeforeSave()
+        {
+            //do someother thing
+        }
+
+        internal override bool Validate()
+        {
+            return true;
+        }
+    }
+}
---
+++ Patterns/Structural/Bridge.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Structural
+{
+    /// <summary>
+    ///Intent of this pattern in to decouple an abstraction from its implementation so that the two can vary independently.
+    /// </summary>
+    /// 
+    abstract class View
+    {
+        protected readonly IResource resource;
+
+        protected View(IResource resource) {
+            this.resource = resource;
+        }
+
+        public abstract string Show();
+    }
+
+    class ListView : View
+    {
+        ListView(IResource resource) 
+            : base(resource) { }
+
+        public override string Show()
+        {
+            return resource.GetTitle() + resource.GetImage();
+        }
+    }
+
+    interface IResource
+    {
+        string GetTitle();
+        string GetImage();
+    }
+
+    class ArtistResource : IResource
+    {
+        Artist artist;
+
+        public ArtistResource(Artist artist)
+        {
+            this.artist = artist;
+        }
+
+        public string GetImage()
+        {
+            return artist.GetPicture();
+        }
+
+        public string GetTitle()
+        {
+            return artist.GetBio();
+        }
+    }
+
+    class Artist
+    {
+        public string GetBio() { return ""; }
+        public string GetPicture() { return "":}
+    }
+}
